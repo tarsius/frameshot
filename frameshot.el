843f0f26137873e07d1c2323470d91cef592afb1
Jonas Bernoulli <jonas@bernoul.li>
2018-07-23 15:12:38 -0500

    Link to examples

--- frameshot.el
+++ frameshot.el
@@ -24,9 +24,10 @@
 
 ;;; Commentary:
 
-;; This package provides a command for taking screenshots of
-;; an individual frame.  It also support setting up the frame
-;; according to simple predetermined rules.
+;; This package provides a command for taking screenshots of an
+;; individual frame.  It also support setting up the frame according
+;; to simple predetermined rules.  Simple examples can be found at
+;; https://github.com/tarsius/emacsair.me/tree/master/assets/readme.
 
 ;; This package uses the `import' and `convert' binaries from
 ;; the `imagemagick' package.
