6a378f2a82b8ea336b8620a12c50386241404c67
Jonas Bernoulli <jonas@bernoul.li>
2022-04-01 12:38:32 +0200

    Convert readme to org

--- frameshot.el
+++ frameshot.el
@@ -1,6 +1,6 @@
 ;;; frameshot.el --- Take screenshots of a frame  -*- lexical-binding: t -*-
 
-;; Copyright (C) 2018-2021  Jonas Bernoulli
+;; Copyright (C) 2018-2022  Jonas Bernoulli
 
 ;; Author: Jonas Bernoulli <jonas@bernoul.li>
 ;; Homepage: https://github.com/tarsius/frameshot
