009ee5f8d754b940c4f86f2276fce401845bb40a
Jonas Bernoulli <jonas@bernoul.li>
2024-06-18 18:03:52 +0200

    Bump compat dependency

--- frameshot.el
+++ frameshot.el
@@ -6,7 +6,7 @@
 ;; Homepage: https://github.com/tarsius/frameshot
 ;; Keywords: multimedia
 
-;; Package-Requires: ((emacs "25.3") (compat "29.1.4.1"))
+;; Package-Requires: ((emacs "25.3") (compat "29.1.4.5"))
 
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 
