050ef28e4007a608f12bbd476ccc52d19854f13d
Jonas Bernoulli <jonas@bernoul.li>
2024-08-05 15:14:49 +0200

    Release version 1.0.0

--- frameshot.el
+++ frameshot.el
@@ -6,7 +6,7 @@
 ;; Homepage: https://github.com/tarsius/frameshot
 ;; Keywords: multimedia
 
-;; Package-Version: 0.2.8
+;; Package-Version: 1.0.0
 ;; Package-Requires: ((emacs "26.1") (compat "30.0.0.0"))
 
 ;; SPDX-License-Identifier: GPL-3.0-or-later
