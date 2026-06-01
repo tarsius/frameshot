6e1f45af13ebaf6ff6f13207c74ed4c5fb8e10c9
Jonas Bernoulli <jonas@bernoul.li>
2026-06-01 17:01:22 +0200

    Release version 1.2.2

--- frameshot.el
+++ frameshot.el
@@ -6,7 +6,7 @@
 ;; Homepage: https://github.com/tarsius/frameshot
 ;; Keywords: multimedia
 
-;; Package-Version: 1.2.1
+;; Package-Version: 1.2.2
 ;; Package-Requires: (
 ;;     (emacs  "28.1")
 ;;     (compat "31.0"))
